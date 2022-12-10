045413d414cd266d5563512ff30b91f1744764bb
Lilyxou <120092763+Lilyxou@users.noreply.github.com>
2022-12-10 16:59:46 +0100

    Update index.js

--- site_web/scripts/index.js
+++ site_web/scripts/index.js
@@ -1,7 +1,7 @@
 let myHeading = document.querySelector('h2');
 myHeading.textContent = "Vite le temps est compté.";
 
-alert("Rendez vous sur la page d'hubble pour en apprendre plus sur hubble. De plus, si vous n'êtes pas sur firefox, veuillez vous y rendre.");
+alert("Rendez vous sur la page d'hubble pour en apprendre plus sur hubble. Ce site n'est malheureuseument pas compatible avec firefox, rdv sur google.");
 alert('Ne cliquez pas sur la case si dessous svp. ça arreterais le jeu.');
 let newtitre = document.querySelector('h1');
 newtitre.textContent = "Rdv sur hubble, lien en bas de cette page.";
